55e7156b23163b3dcc3e1817abd399ce5a5d6f06
Nitin Piparava <ndpiparava@gmail.com>
2025-09-17 00:43:54 +0200

    ORCA-000 feat : FE theme and style setup

---
+++ Orca/src/themes/ColorType.ts
@@ -0,0 +1,10 @@
+enum ColorType {
+  white = '#FFFFFF',
+  offWhite = '#f6f6f6',
+  black = '#0000004D',
+  mildOrange = '#FF7F7F',
+  primary = '#77afe8e5',
+  secondary = '#77AFE880',
+}
+
+export default ColorType;
